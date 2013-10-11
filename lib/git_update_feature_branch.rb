6f02606eefdd8131610ba1ce397a3ea7e9a9548d
Andreas Gnyp <agnyp@friendscout24.de>
2013-10-11 21:56:42 +0200

    Refactoring of script-file
    start to refactor the big script-file
    into gem-schema

--- lib/git_update_feature_branch.rb
+++ lib/git_update_feature_branch.rb
@@ -1,5 +1,5 @@
 require "git_update_feature_branch/version"
+require "git_update_feature_branch/base"
 
 module GitUpdateFeatureBranch
-  # Your code goes here...
 end
---
+++ lib/git_update_feature_branch/git_interface.rb
@@ -0,0 +1,18 @@
+module GitInterface
+  def checkout_branch(branch)
+    `git checkout #{branch}`;err=$?.success?
+    if !err
+      puts "branch not known. aborting ..."
+      exit 1
+    end
+  end
+
+  def exec_git(command, error_text = "Error ...")
+    out=`#{command}`;!suc=$?.success?
+    unless suc
+      puts error_text
+      exit 1
+    end
+    out
+  end
+end
