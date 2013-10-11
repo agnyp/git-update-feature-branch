c12be72dbbfb1a3c56c0866cd7e221683106e378
Andreas Gnyp <agnyp@friendscout24.de>
2013-10-11 18:22:44 +0200

    init bundle gem git_update_feature_branch
    In order to use this as a gem
    
    create a lot of files

---
+++ lib/git_update_feature_branch.rb
@@ -0,0 +1,5 @@
+require "git_update_feature_branch/version"
+
+module GitUpdateFeatureBranch
+  # Your code goes here...
+end
---
+++ lib/git_update_feature_branch/version.rb
@@ -0,0 +1,3 @@
+module GitUpdateFeatureBranch
+  VERSION = "0.0.1"
+end
