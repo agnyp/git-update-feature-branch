167a1056b8bd6ed1d487d096621ac1f9b1b5dbd1
Andreas Gnyp <andreas@gnyp.de>
2013-10-13 01:43:26 +0200

    Tests fo RemoteBranchManager

---
+++ test/remote_branch_manager_spec.rb
@@ -0,0 +1,17 @@
+
+  context '#push_branch' do
+    context 'new branch is highest branch' do
+      it 'should push branch' 
+    end
+
+    context 'new branch is not highest branch' do
+      it 'should raise NewerBranchException'
+    end
+
+    context 'after pushing: Check for higher branch -> there is none' do
+      it 'should return all good'
+    end 
+
+    context 'after pushing: Check for higher branch -> there is one!' do
+      it 'should raise NewerBranchException'
+    end 
