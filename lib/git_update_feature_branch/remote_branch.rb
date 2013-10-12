94c4ddb4521c6ba68fdaaf555cf96fe562e3c1ed
Andreas Gnyp <andreas@gnyp.de>
2013-10-13 01:45:03 +0200

    Implementing tests for RemoteBranch
    done latest_remote_branch
    done list_remote_branches

--- lib/git_update_feature_branch/remote_branch.rb
+++ lib/git_update_feature_branch/remote_branch.rb
@@ -1,37 +1,48 @@
 require 'git_update_feature_branch/git_interface'
 
 class RemoteBranch
+  class << self
 
-  extend GitInterface
+    extend GitInterface
 
-  def self.list_remote_branches
-    exec_git "git fetch", "Could not fetch. Aborting..."
-    remote_branches = exec_git "git branch -r", "Cannot find remote branches. Aborting..."
-    remote_branches = find_branches remote_branches
+    def list_remote_branches
+      exec_git "git fetch", "Could not fetch. Aborting..."
+      remote_branches = exec_git "git branch -r", "Cannot find remote branches. Aborting..."
+      remote_branches = find_branches remote_branches
+    end
+
+    def latest_remote_branch
+      branches = list_remote_branches 
+      return branches if branches.size <= 1
+      highest = -1
+      branches.each_with_index{ |br, ind|
+        if br.split('__').last.to_i > highest
+          highest = ind
+        end
+      }
+      [branches[highest]]
+    end
+
+    def delete_updated
+    end
+
+    def strip_branches(branches)
+      branches.split(/\n/).map{ |item|
+        item.gsub(/\s+/,'') 
+      }.reject{|item|
+        # not interested in other remotes than $origin and also not in HEAD
+        !item.match(/\A#{$origin}\//) or item.include?('HEAD')
+      }.reject{|item|
+        !item.include?($branch)
+      }
+    end
+
+    def find_branches(str_list)
+      strip_branches(str_list).reject{ |item|
+        !item.match(/\A#{$origin}\/fb__/) or !item.match(/__\d+\Z/)
+      }
+    end
+
   end
-
-  def self.delete_updated
-  end
-
-
-  private 
-
-  def self.strip_branches(branches)
-    branches.split(/\n/).map{ |item|
-      item.gsub(/\s+/,'') 
-    }.reject{|item|
-      # not interested in other remotes than $origin and also not in HEAD
-      !item.match(/\A#{$origin}\//) or item.include?('HEAD')
-    }.reject{|item|
-      !item.include?($branch)
-    }
-  end
-  
-  def self.find_branches(str_list)
-    strip_branches(str_list).reject{ |item|
-      !item.match(/\A#{$origin}\/fb__/) or !item.match(/__\d+\Z/)
-    }
-  end
-
 end
 
--- test/remote_branch_spec.rb
+++ test/remote_branch_spec.rb
@@ -1,24 +1,22 @@
 require 'git_update_feature_branch/remote_branch'
 
 
-describe RemoteBranch, '#list_remote_branches' do
+describe RemoteBranch do
 
-before :all do
-@casual_branches = <<BR
+  before :all do
+    @casual_branches = <<BR
   origin/HEAD -> origin/master
   origin/as-gem
   origin/continue
   origin/master
 BR
-
-@correct_fb = <<BR
+    @correct_fb = <<BR
   origin/HEAD -> origin/master
   origin/fb__feature-branch__0
   origin/continue
   origin/master
 BR
-
-@incorrect_fbs = <<BR
+    @incorrect_fbs = <<BR
   origin/HEAD -> origin/master
   origin/feature-branch__0
   origin/fb__feature-branch_
@@ -28,8 +26,7 @@ BR
   origin/continue
   origin/master
 BR
-
-@three_fbs = <<BR
+    @three_fbs = <<BR
   origin/HEAD -> origin/master
   origin/fb__feature-branch__0
   origin/fb__feature-branch__1
@@ -37,85 +34,135 @@ BR
   origin/continue
   origin/master
 BR
-  $origin = 'origin'
+    @random_fbs = <<BR
+  origin/HEAD -> origin/master
+  origin/fb__feature-branch__154
+  origin/fb__feature-branch__153
+  origin/continue
+  origin/fb__feature-branch__0
+  origin/master
+  origin/fb__feature-branch__156
+BR
 
-end
-
-  context 'no remote branch' do
-    before do
-      $branch = "notInThere"
-      RemoteBranch.stub(:exec_git).with("git fetch", kind_of(String))
-      RemoteBranch.stub(:exec_git).with("git branch -r", kind_of(String)).and_return(@casual_branches)
-    end
-    subject {RemoteBranch.list_remote_branches}
-    it 'should show an empty array' do
-      subject.should be_empty
-    end
+  end
+  before :each do
+    $origin = 'origin'
+    $branch = "feature-branch"
   end
 
-  context 'one correct remote branch' do
-    before do
-      $branch = "feature-branch"
-      RemoteBranch.stub(:exec_git).with("git fetch", kind_of(String))
-      RemoteBranch.stub(:exec_git).with("git branch -r", kind_of(String)).and_return(@correct_fb)
+  context '#list_remote_branches:' do
+
+    context 'no remote branch' do
+      before do
+        $branch = "notInThere"
+        RemoteBranch.stub(:exec_git).with("git fetch", kind_of(String))
+        RemoteBranch.stub(:exec_git).with("git branch -r", kind_of(String)).and_return(@casual_branches)
+      end
+      subject {RemoteBranch.list_remote_branches}
+      it 'should show an empty array' do
+        subject.should be_empty
+      end
     end
-    subject {RemoteBranch.list_remote_branches}
-    it 'should have one correct branch in array' do
-      subject[0].should eql("#{$origin}/fb__#{$branch}__0")
+
+    context 'one correct remote branch' do
+      before do
+        RemoteBranch.stub(:exec_git).with("git fetch", kind_of(String))
+        RemoteBranch.stub(:exec_git).with("git branch -r", kind_of(String)).and_return(@correct_fb)
+      end
+      subject {RemoteBranch.list_remote_branches}
+      it 'should have one correct branch in array' do
+        subject.size.should eql(1)
+        subject[0].should eql("#{$origin}/fb__#{$branch}__0")
+      end
+
     end
-    
+
+    context 'a lot of incorrect remote branches' do
+      before do
+        RemoteBranch.stub(:exec_git).with("git fetch", kind_of(String))
+        RemoteBranch.stub(:exec_git).with("git branch -r", kind_of(String)).and_return(@incorrect_fbs)
+      end
+      subject {RemoteBranch.list_remote_branches}
+      it 'should be empty' do
+        subject.should be_empty
+      end
+
+    end
+
+    context '3 remote branches' do
+      before do
+        RemoteBranch.stub(:exec_git).with("git fetch", kind_of(String))
+        RemoteBranch.stub(:exec_git).with("git branch -r", kind_of(String)).and_return(@three_fbs)
+      end
+      subject {RemoteBranch.list_remote_branches}
+      it 'should have 3 branches in array' do
+        subject.size.should eql(3)
+      end
+    end
+
   end
 
-  context 'a lot of incorrect remote branches' do
-    before do
-      $branch = "feature-branch"
-      RemoteBranch.stub(:exec_git).with("git fetch", kind_of(String))
-      RemoteBranch.stub(:exec_git).with("git branch -r", kind_of(String)).and_return(@incorrect_fbs)
+  context '#latest_remote_branch' do
+
+    context 'there are 3 branches' do
+      before do
+        RemoteBranch.stub(:exec_git).with("git fetch", kind_of(String))
+        RemoteBranch.stub(:exec_git).with("git branch -r", kind_of(String)).and_return(@three_fbs)
+      end
+      subject {RemoteBranch.latest_remote_branch}
+      it 'should give one branch with the highest number' do
+        subject.size.should eql(1)
+        subject[0].should eql("#{$origin}/fb__#{$branch}__2")
+      end
     end
-    subject {RemoteBranch.list_remote_branches}
-    it 'should be empty' do
-      subject.should be_empty
+
+    context 'there are 3 non-consecutive branches' do
+      before do
+        RemoteBranch.stub(:exec_git).with("git fetch", kind_of(String))
+        RemoteBranch.stub(:exec_git).with("git branch -r", kind_of(String)).and_return(@random_fbs)
+      end
+      subject {RemoteBranch.latest_remote_branch}
+      it 'should give one branch with the highest number' do
+        subject.size.should eql(1)
+        subject[0].should eql("#{$origin}/fb__#{$branch}__156")
+      end
     end
-    
+
+    context 'there is one branch' do
+      before do
+        RemoteBranch.stub(:exec_git).with("git fetch", kind_of(String))
+        RemoteBranch.stub(:exec_git).with("git branch -r", kind_of(String)).and_return(@correct_fb)
+      end
+      subject {RemoteBranch.latest_remote_branch}
+      it 'should give back this one branch' do
+        subject.size.should eql(1)
+        subject[0].should eql("#{$origin}/fb__#{$branch}__0")
+      end
+    end
+
+    context 'there is no branch' do
+      before do
+        RemoteBranch.stub(:exec_git).with("git fetch", kind_of(String))
+        RemoteBranch.stub(:exec_git).with("git branch -r", kind_of(String)).and_return(@incorrect_fbs)
+      end
+      subject {RemoteBranch.latest_remote_branch}
+      it 'should give back empty array' do
+        subject.should be_empty
+      end
+    end
+
   end
 
-  context '3 remote branches'do
-    before do
-      $branch = "feature-branch"
-      RemoteBranch.stub(:exec_git).with("git fetch", kind_of(String))
-      RemoteBranch.stub(:exec_git).with("git branch -r", kind_of(String)).and_return(@three_fbs)
+  context '#push_branch' do
+
+    context 'no errors' do
+      it 'should just do it'
     end
-    subject {RemoteBranch.list_remote_branches}
-    it 'should have 3 branches in array' do
-      subject.size.should eql(3)
-    end
+
+    context 'branch already exists' do
+      it 'should raise BranchExistsException'
+    end 
+
   end
-end
-
-describe RemoteBranch, '#working_branch' do
-  context 'given 2 remote branches' do
-    before do
-
-    end
-    it 'should remember the highest branch'
-  end
-end
-
-describe RemoteBranch, '#push_branch' do
-  context 'new branch is highest branch' do
-    it 'should push branch' 
-  end
-
-  context 'new branch is not highest branch' do
-    it 'should raise NewerBranchException'
-  end
-
-  context 'after pushing: Check for higher branch -> there is none' do
-    it 'should return all good'
-  end 
-
-  context 'after pushing: Check for higher branch -> there is one!' do
-    it 'should raise NewerBranchException'
-  end 
 
 end
